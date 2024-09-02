b118b2cd3169a0ec406f914e9df837ff44eab4de
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2024-09-02 21:49:45 +0600

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="body-section w-[1099px] m-auto">
+    <div
+      class="container main_wbsite bg-[#ffffff] pr-[15px] pl-[15px] mr-auto ml-auto text-[17px]"
+    >
+      <home-navbar></home-navbar>
+      <slot></slot>
+      <footer-section></footer-section>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.body-section {
+  font-family: SolaimanLipiNormal;
+  line-height: 1.42857143;
+  box-sizing: border-box;
+}
+
+.main_wbsite {
+  box-shadow: 0 0 20px #ddd;
+  line-height: 1.42857143;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,308 @@
+<template>
+  <div>
+    <h1 class="text-blue-700">siam</h1>
+    <div class="flex items-center justify-between p-4">
+      <div class="w-1/4">
+        <a href="https://royelnews.com">
+          <img
+            src="https://royelnews.com/wp-content/uploads/2022/12/277830472_318193127061596_4636589903513880359_n-1.png"
+            alt="Logo"
+            class="w-full"
+          />
+        </a>
+      </div>
+      <div class="w-3/4 flex justify-end">
+        <a
+          href="https://www.facebook.com/profile.php?id=100083300141120&sk=reviews"
+          target="_blank"
+        >
+          <img
+            src="https://royelnews.com/wp-content/uploads/2024/04/UntiegfuayugJDSgauri84yhtiewtr7wuk950-265rwaifioet7r8.jpg"
+            alt="Banner"
+            class="w-full"
+          />
+        </a>
+      </div>
+    </div>
+
+    <!-------menu option stsrt-------->
+
+    <div class="menu-main-menu flex flex-col mx-w-[1099px] m-auto">
+      <nav class="bg-[#d60909] shadow">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-6">
+          <div class="flex justify-between h-[50px]">
+            <div class="flex items-center">
+              <button
+                class="text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700 sm:hidden"
+                aria-label="Toggle navigation"
+              >
+                <svg
+                  class="h-6 w-6"
+                  stroke="currentColor"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    class="inline-flex"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  />
+                </svg>
+              </button>
+              <div class="hidden sm:flex space-x-3 text-[14px]">
+                <a
+                  href="https://royelnews.com/"
+                  class="text-white hover:text-white bg-[#73337D] inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                  title="প্রচ্ছদ"
+                  >প্রচ্ছদ</a
+                >
+                <div id="menu-item-82" class="relative group">
+                  <a
+                    title="সারাদেশ"
+                    href="#"
+                    class="flex py-2 px-3 text-white hover:text-white hover:bg-[#9A1515]"
+                  >
+                    সারাদেশ
+                    <svg
+                      class="ml-2 h-5 w-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M5.23 7.21a.75.75 0 011.06.02L10 10.77l3.71-3.54a.75.75 0 111.04 1.08l-4.25 4a.75.75 0 01-1.04 0l-4.25-4a.75.75 0 01-.02-1.06z"
+                        clip-rule="evenodd"
+                      /></svg
+                    ><span class="caret"></span>
+                  </a>
+
+                  <!-- Dropdown Menu -->
+                  <div
+                    class="absolute left-0 hidden bg-[#9A1515] w-[190px] group-hover:block"
+                  >
+                    <div class="menu-item">
+                      <a
+                        title="ঢাকা"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%95%e0%a7%83%e0%a6%b7%e0%a6%bf/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        ঢাকা
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="রংপুর"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%a7%e0%a6%b0%e0%a7%8d%e0%a6%ae/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        রংপুর
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="চট্রগ্রাম"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%ad%e0%a7%8d%e0%a6%b0%e0%a6%ae%e0%a6%a3/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        চট্রগ্রাম
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="রাজশাহী"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%b2%e0%a6%be%e0%a6%87%e0%a6%ab%e0%a6%b8%e0%a7%8d%e0%a6%9f%e0%a6%be%e0%a6%87%e0%a6%b2/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        রাজশাহী
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="সিলেট"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%b8%e0%a6%ae%e0%a7%8d%e0%a6%aa%e0%a6%be%e0%a6%a6%e0%a6%95%e0%a7%80%e0%a7%9f/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        সিলেট
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="ময়মনসিংহ"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%b8%e0%a7%8d%e0%a6%ac%e0%a6%be%e0%a6%b8%e0%a7%8d%e0%a6%a5%e0%a7%8d%e0%a6%af-%e0%a6%93-%e0%a6%9a%e0%a6%bf%e0%a6%95%e0%a6%bf%e0%a7%8e%e0%a6%b8%e0%a6%be/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        ময়মনসিংহ
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="বরিশাল"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%b8%e0%a7%8d%e0%a6%ac%e0%a6%be%e0%a6%b8%e0%a7%8d%e0%a6%a5%e0%a7%8d%e0%a6%af-%e0%a6%93-%e0%a6%9a%e0%a6%bf%e0%a6%95%e0%a6%bf%e0%a7%8e%e0%a6%b8%e0%a6%be/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        বরিশাল
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="খুলনা"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%b8%e0%a7%8d%e0%a6%ac%e0%a6%be%e0%a6%b8%e0%a7%8d%e0%a6%a5%e0%a7%8d%e0%a6%af-%e0%a6%93-%e0%a6%9a%e0%a6%bf%e0%a6%95%e0%a6%bf%e0%a7%8e%e0%a6%b8%e0%a6%be/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        খুলনা
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <a
+                  href="https://royelnews.com/category/%e0%a6%9c%e0%a6%be%e0%a6%a4%e0%a7%80%e0%a7%9f/"
+                  class="text-white hover:text-white hover:bg-[#9A1515] inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  title="জাতীয়"
+                  >জাতীয়</a
+                >
+                <a
+                  href="https://royelnews.com/category/%e0%a6%b0%e0%a6%be%e0%a6%9c%e0%a6%a8%e0%a7%80%e0%a6%a4%e0%a6%bf/"
+                  class="text-white hover:text-white hover:bg-[#9A1515] inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  title="রাজনীতি"
+                  >রাজনীতি</a
+                >
+                <a
+                  href="https://royelnews.com/category/%e0%a6%86%e0%a6%87%e0%a6%a8-%e0%a6%86%e0%a6%a6%e0%a6%be%e0%a6%b2%e0%a6%a4/"
+                  class="text-white hover:text-white hover:bg-[#9A1515] inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  title="অর্থ ও বাণিজ্য"
+                  >অর্থ ও বাণিজ্য</a
+                >
+                <a
+                  href="https://royelnews.com/category/%e0%a6%85%e0%a6%b0%e0%a7%8d%e0%a6%a5%e0%a6%a8%e0%a7%80%e0%a6%a4%e0%a6%bf/"
+                  class="text-white hover:text-white hover:bg-[#9A1515] inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  title="অর্থনীতি"
+                  >আর্ন্তজাতিক</a
+                >
+                <a
+                  href="https://royelnews.com/category/%e0%a6%b8%e0%a6%ae%e0%a7%8d%e0%a6%aa%e0%a6%be%e0%a6%a6%e0%a6%95%e0%a7%80%e0%a7%9f/"
+                  class="text-white hover:text-white hover:bg-[#9A1515] inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  title="চাকরি"
+                  >চাকরি</a
+                >
+                <a
+                  href="https://royelnews.com/category/%e0%a6%b8%e0%a7%8d%e0%a6%ac%e0%a6%be%e0%a6%b8%e0%a7%8d%e0%a6%a5%e0%a7%8d%e0%a6%af/"
+                  class="text-white hover:text-white hover:bg-[#9A1515] inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  title="বিনোদন"
+                  >বিনোদন</a
+                >
+                <a
+                  href="https://royelnews.com/category/%e0%a6%b6%e0%a6%bf%e0%a6%95%e0%a7%8d%e0%a6%b7%e0%a6%be/"
+                  class="text-white hover:text-white hover:bg-[#9A1515] inline-flex items-center px-1 pt-1 text-sm font-medium"
+                  title="শিক্ষা"
+                  >শিক্ষা</a
+                >
+                <a
+                  href="https://royelnews.com/category/%e0%a6%ab%e0%a7%87%e0%a6%9a%e0%a6%be%e0%a6%b0/"
+                  class="inline-flex items-center px-1 pt-1 text-sm font-medium text-white hover:text-white hover:bg-[#9A1515]"
+                  title="শিল্প ও সাহিত্য"
+                  >শিল্প ও সাহিত্য</a
+                >
+
+                <!-- Parent Menu Item -->
+                <div id="menu-item-82" class="relative group">
+                  <a
+                    title="অন্যান্য"
+                    href="#"
+                    class="flex py-2 px-4 text-white hover:text-white hover:bg-[#9A1515]"
+                  >
+                    অন্যান্য
+                    <svg
+                      class="ml-2 h-5 w-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M5.23 7.21a.75.75 0 011.06.02L10 10.77l3.71-3.54a.75.75 0 111.04 1.08l-4.25 4a.75.75 0 01-1.04 0l-4.25-4a.75.75 0 01-.02-1.06z"
+                        clip-rule="evenodd"
+                      /></svg
+                    ><span class="caret"></span>
+                  </a>
+
+                  <!-- Dropdown Menu -->
+                  <div
+                    class="absolute left-0 hidden bg-[#9A1515] w-[170px] group-hover:block"
+                  >
+                    <div class="menu-item">
+                      <a
+                        title="কৃষি"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%95%e0%a7%83%e0%a6%b7%e0%a6%bf/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        কৃষি
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="ধর্ম"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%a7%e0%a6%b0%e0%a7%8d%e0%a6%ae/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        ধর্ম
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="ভ্রমণ"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%ad%e0%a7%8d%e0%a6%b0%e0%a6%ae%e0%a6%a3/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        ভ্রমণ
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="লাইফস্টাইল"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%b2%e0%a6%be%e0%a6%87%e0%a6%ab%e0%a6%b8%e0%a7%8d%e0%a6%9f%e0%a6%be%e0%a6%87%e0%a6%b2/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        লাইফস্টাইল
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="সম্পাদকীয়"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%be%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%b8%e0%a6%ae%e0%a7%8d%e0%a6%aa%e0%a6%be%e0%a6%a6%e0%a6%95%e0%a7%80%e0%a7%9f/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        সম্পাদকীয়
+                      </a>
+                    </div>
+                    <div class="menu-item">
+                      <a
+                        title="স্বাস্থ্য ও চিকিৎসা"
+                        href="https://royelnews.com/category/%e0%a6%85%e0%a6%a8%e0%a7%8d%e0%a6%af%e0%a6%a8%e0%a7%8d%e0%a6%af/%e0%a6%b8%e0%a7%8d%e0%a6%ac%e0%a6%be%e0%a6%b8%e0%a7%8d%e0%a6%a5%e0%a7%8d%e0%a6%af-%e0%a6%93-%e0%a6%9a%e0%a6%bf%e0%a6%95%e0%a6%bf%e0%a7%8e%e0%a6%b8%e0%a6%be/"
+                        class="block py-2 px-4 text-white hover:text-gray-800 hover:bg-[#DCDCDC]"
+                      >
+                        স্বাস্থ্য ও চিকিৎসা
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.menu-main-menu {
+  font-size: 16px;
+}
+</style>
