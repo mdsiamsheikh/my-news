f5eba2615959ee75562859da6af0b96936db7edc
mdsiamsheikh <mdsiamsheikh6640@gmail.com>
2024-09-03 15:38:19 +0600

    add a footer section

---
+++ components/footer-section.vue
@@ -0,0 +1,117 @@
+<template>
+  <!--===================  footer ====================-->
+  <div>
+    <div class="bg-[#4F4F4F] h-[244px] text-white py-7 mt-3">
+      <div
+        class="container mx-auto flex flex-wrap justify-between items-center border-b border-gray-700 pb-4"
+      >
+        <div
+          class="w-full md:w-1/3 flex justify-center md:justify-start mb-4 md:mb-0"
+        >
+          <a href="https://royelnews.com">
+            <img
+              src="https://royelnews.com/wp-content/uploads/2022/12/277830472_318193127061596_4636589903513880359_n-1.png"
+              alt="Logo"
+              class="max-w-full"
+            />
+          </a>
+        </div>
+        <div
+          class="w-full md:w-2/3 flex justify-center md:justify-end p-4 py-5"
+        >
+          <ul class="flex flex-wrap space-x-6">
+            <li class="p-[5px] border border-gray-900">
+              <a href="" class="text-[#252624] text-[16px] hover:text-[#F4C819]"
+                >Home</a
+              >
+            </li>
+            <li class="p-[5px] border border-gray-900">
+              <a href="" class="text-[#252624] text-[16px] hover:text-[#F4C819]"
+                >About Us</a
+              >
+            </li>
+            <li class="p-[5px] border border-gray-900">
+              <a href="" class="text-[#252624] text-[16px] hover:text-[#F4C819]"
+                >Contact Us</a
+              >
+            </li>
+            <li class="p-[5px] border border-gray-900">
+              <a href="" class="text-[#252624] text-[16px] hover:text-[#F4C819]"
+                >Privacy Policy</a
+              >
+            </li>
+            <li class="p-[5px] border border-gray-900">
+              <a href="" class="text-[#252624] text-[16px] hover:text-[#F4C819]"
+                >বাংলা Converter</a
+              >
+            </li>
+            <li class="p-[5px] border border-gray-900 mt-3">
+              <a href="" class="text-[#252624] text-[16px] hover:text-[#F4C819]"
+                >উলিপুরে হাতিয়া গণহত্যা দিবস পালিত</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div
+        class="container mx-auto flex flex-wrap justify-between items-center pt-4 p-2"
+      >
+        <div
+          class="w-full md:w-1/2 text-center md:text-left mb-4 md:mb-0 text-[#252624] text-[16px]"
+        >
+          <p><strong>প্রকাশক ও সম্পাদক</strong>: আরিফুল ইসলাম</p>
+        </div>
+        <div
+          class="w-full md:w-1/2 text-center md:text-right text-[#252624] text-[16px]"
+        >
+          <p>সরকারের অনুমোদিত একটি অনলাইন পত্রিকা, রেজিস্ট্রেশন নাম্বার 1243</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="root bg-[#262626] p-5">
+      <div class="flex justify-between items-center">
+        <div class="text-right">
+          <p class="text-gray-400">
+            © All rights reserved © 2023-2024 siam.com
+          </p>
+        </div>
+        <div class="text-left">
+          <p class="text-gray-400">
+            ডিজাইন ও কারিগরি সহযোগিতায়:
+            <a
+              href=""
+              target="_blank"
+              title="Mobile : 01744977947"
+              class="text-[#e6ce6e]"
+            >
+              Hostitbd.Com
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <!-- Scroll to top button -->
+      <a
+        href="#"
+        class="scrollToTop fixed bottom-4 right-4 text-[#CC0000] border border-[#CC0000] p-4 rounded-full shadow-lg"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          class="w-4 h-4"
+          fill="currentColor"
+        >
+          <path
+            d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"
+          />
+        </svg>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
